0645c88fc087db339ffc042e940401c65ce51962
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-22 14:11:56 -0300

    Criar entidade lead

---
+++ src/main/java/br/com/zup/LeadCollector/Lead/Lead.java
@@ -0,0 +1,50 @@
+package br.com.zup.LeadCollector.Lead;
+
+import br.com.zup.LeadCollector.Produto.Produto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "leads")
+public class Lead {
+    @Id
+    private String email;
+    @Column(columnDefinition = "VARCHAR(100) DEFAULT 'NAO INFORMADO'")
+    private String nome;
+    private List<Produto> produtosDesejados;
+
+    public Lead(String email, String nome, List<Produto> produtosDesejados) {
+        this.email = email;
+        this.nome = nome;
+        this.produtosDesejados = produtosDesejados;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Produto> getProdutosDesejados() {
+        return produtosDesejados;
+    }
+
+    public void setProdutosDesejados(List<Produto> produtosDesejados) {
+        this.produtosDesejados = produtosDesejados;
+    }
+
+}
