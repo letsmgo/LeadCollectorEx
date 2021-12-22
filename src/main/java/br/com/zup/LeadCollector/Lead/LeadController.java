b7f48722fc755e36825cdae61bf298dd1c041906
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-22 14:29:44 -0300

    Criar método de cadastrar lead na controller

--- src/main/java/br/com/zup/LeadCollector/Lead/LeadController.java
+++ src/main/java/br/com/zup/LeadCollector/Lead/LeadController.java
@@ -1,10 +1,11 @@
 package br.com.zup.LeadCollector.Lead;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.Access;
 
 @RestController
 @RequestMapping("/leads")
@@ -12,5 +13,9 @@ public class LeadController {
     @Autowired
     private LeadService leadService;
 
+    @PostMapping
+    public Lead cadastrarLead(@RequestBody Lead lead){
+        return leadService.cadastrarLead(lead);
+    }
 
 }
